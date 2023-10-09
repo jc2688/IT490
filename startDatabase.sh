b4f5669efcb0c395ea8119c1b7532acaee4fc4c9
jc2688 <95941616+jc2688@users.noreply.github.com>
2023-10-09 21:49:32 +0000

    Update startDatabase.sh

--- startDatabase.sh
+++ startDatabase.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#check if instance is already on
-sudo aws configure
 #check if it is already online
-if sudo aws rds wait db-instance-available --db-intance-identifier dbit490-fix --region us-east-1; then
-        echo "The RDS server is already online."
+if systemctl is-active --quiet mysql; then
+        echo "MySQL server is already online."
         exit 0
 fi
-#turn on RDS database
-sudo aws reds start-bt-instance --db-instance-identifier dbit490-fixed --region us-east-1;
-#check if it actually turned on
-if sudo aws reds wait db-instance-available --db-instance-identifier dbit490-fixed --region us-east-1; then
-        echo "The RDS server is active!"
+#Start MySQL Server
+sudo service mysql start
+
+#check if it has turned on now
+if systemctl is-active --quiet mysql; then
+        echo "MySQL server has been started"
+        exit 0
+else
+        echo "MySQL failed to start"
         exit 1
 fi
