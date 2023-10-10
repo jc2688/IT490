e2672ac5c5e33b22ce43827a54ff6bc4a6c66050
jc2688 <95941616+jc2688@users.noreply.github.com>
2023-10-09 22:47:30 -0400

    Update checkOtherServices.sh

--- checkOtherServices.sh
+++ checkOtherServices.sh
@@ -3,7 +3,7 @@
 #checks  rabbit MQ
 if ping 10.244.1.4 -c 1; then
 	#checks if rabbitMQ is on
-	sudo ssh vboxuser@10.244.1.4 './start_rabbitmq.sh'
+	ssh -t vboxuser@10.244.1.4 'sudo ./start_rabbitmq.sh'
 
 else
 	echo "Rabbit MQ Host not online"
@@ -13,7 +13,7 @@ fi
 #checks back-end 1
 if ping 10.244.1.3 -c 1; then
 	#check if NGINX is on
-	sudo ssh user@10.244.1.3 './back-start.sh'
+	ssh -t user@10.244.1.3 'sudo ./back-start.sh'
 
 else
 	echo "NGINX Host not online"
@@ -23,7 +23,7 @@ fi
 #checks back-end 2
 if ping 10.244.1.5 -c 1; then
 	#check if NGINX is on
-	sudo ssh user@10.244.1.5 './back-start.sh'
+	ssh -t user@10.244.1.5 './sudo back-start.sh'
 
 else
 	echo "NGINX HOST not Online"
@@ -32,8 +32,15 @@ fi
 #checks database
 if ping 10.244.1.2 -c 1; then
 	#check if  MySQL is on
-	sudo ssh jc268@10.244.1.2 'cd IT490 ; ./startDatabase.sh'
+	ssh -t jc268@10.244.1.2 'cd IT490 ; sudo ./startDatabase.sh'
 
 else
 	echo "MySQL Host not online"
+fi
+#checks front end
+if ping 10.244.1.6 -c 1; then
+	#check if NGINX Front edn is on
+ 	shh -t sea@10.244.1.6 'sudo ./startwebsite.sh'
+else
+	echo "NGINX Host Front End is Offline"
 fi
