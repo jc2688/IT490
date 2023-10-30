72cc0cb868de92fac0209fc5976d4f50b782de5c
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2023-10-30 18:33:52 -0400

    real first test

--- Frontend/it490website/server/index.js
+++ Frontend/it490website/server/index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const amqp = require('amqplib/callback_api'); // Added amqp import
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/homescreen', async (req, res) => { // Adjusted route
+const connectToRabbitMQ = async () => {
   try {
-    // Simulating a successful connection to RabbitMQ
-    const connection = true;
-    const channel = true;
+    const connection = await amqp.connect('amqp://10.244.1.4'); // Connect to RabbitMQ server
+    const channel = await connection.createChannel(); // Create a channel
+    
+    const queue = 'dbQueue'; // Specify the queue name
 
-    if (!connection || !channel) {
-      return res.status(500).json({ error: 'Failed to connect to RabbitMQ' });
-    }
+    // Assert the queue, this makes sure the queue exists
+    channel.assertQueue(queue, { durable: false });
+    
+    return channel; // Return the channel for use in sending messages
+  } catch (error) {
+    console.error('Error connecting to RabbitMQ:', error);
+    return null;
+  }
+};
 
+app.post('/homescreen', async (req, res) => {
+  try {
     const { type, message } = req.body;
 
     const dbQueueMessage = JSON.stringify({ type, message });
+
+    const channel = await connectToRabbitMQ();
+    if (channel) {
+      channel.sendToQueue('dbQueue', Buffer.from(dbQueueMessage)); // Send message to RabbitMQ
+    }
 
     return res.json({ success: true, dbQueueMessage });
   } catch (error) {
