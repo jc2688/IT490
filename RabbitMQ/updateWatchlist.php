3eb0fefc5aff5a35a9f04c30083d296ea1da3095
Brendan Powers <snp76@njit.edu>
2023-11-17 17:54:16 -0500

    500 fix, just filename

--- RabbitMQ/updateWatchlist.php
+++ RabbitMQ/updateWatchlist.php
@@ -1,7 +1,7 @@
 <?php
-require_once(__DIR__ . '/server/path.inc');
-require_once(__DIR__ . '/server/get_host_info.inc');
-require_once(__DIR__ . '/server/rabbitMQLib.inc');
+require_once(__DIR__ . '/path.inc');
+require_once(__DIR__ . '/get_host_info.inc');
+require_once(__DIR__ . '/rabbitMQLib.inc');
 
 if ($_POST){
     $request = array();
