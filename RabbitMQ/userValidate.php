3f6adc26218179da7646c49f46d695f253486101
Brendan Powers <snp76@njit.edu>
2023-11-17 18:02:56 -0500

    500 fix, / filename

--- RabbitMQ/userValidate.php
+++ RabbitMQ/userValidate.php
@@ -1,7 +1,7 @@
 <?php
-require_once(__DIR__ . 'path.inc');
-require_once(__DIR__ . 'get_host_info.inc');
-require_once(__DIR__ . 'rabbitMQLib.inc');
+require_once(__DIR__ . '/path.inc');
+require_once(__DIR__ . '/get_host_info.inc');
+require_once(__DIR__ . '/rabbitMQLib.inc');
 
 if ($_POST){
   $request = array();
