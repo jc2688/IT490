7b02d1b3de07f03ca7053af5eb810a3aa4d6a058
Isfar Ehsan <118141419+ie42@users.noreply.github.com>
2023-12-17 20:35:37 -0500

    Update testAPI.php

--- testAPI.php
+++ testAPI.php
@@ -1,119 +1,108 @@
 <?php
-
 $apiKey = 'ab0f6b84bbcbbd4066f4fee3eaba248c';
 
-
-// Function to get popular recommendations from the TMDB API
-function getPopularRecommendations() {
-    global $apiKey; // Use the global API key variable
-    
-    // Make a request to TMDB API for popular recommendations
-    $url = "https://api.themoviedb.org/3/trending/all/day?api_key={$apiKey}";
-
-    // Use file_get_contents to make the request
-    $response = file_get_contents($url);
-
-    // Decode the JSON response
-    $data = json_decode($response, true);
-
-    // Check if there are results
-    if (isset($data['results']) && count($data['results']) > 0) {
-        // Extract information from the first item
-        $result = $data['results'][0];
-
-        // Return relevant information
-        return [
-            'title' => $result['title'] ?? $result['name'],
-            'media_type' => $result['media_type'],
-            'poster_path' => $result['poster_path'] ?? '/default_poster.jpg', // Use a default poster path if not available
-        ];
-    } else {
-        // If no results are found, return a default result
-        return [
-            'title' => 'No Recommendations Found',
-            'media_type' => 'movie', // or 'tv'
-            'poster_path' => '/default_poster.jpg',
-        ];
-    }
-}
-
-// Function to get recommendations based on a specific movie ID and media type from the TMDB API
-function getRecommendationByMovieId($movieId, $mediaType) {
-    global $apiKey; // Use the global API key variable
-    
-    // Make a request to TMDB API for movie recommendations
-    $url = "https://api.themoviedb.org/3/{$mediaType}/{$movieId}/recommendations?api_key={$apiKey}&language=en-US&page=1";
-
-    // Use file_get_contents to make the request
-    $response = file_get_contents($url);
-
-    // Decode the JSON response
-    $data = json_decode($response, true);
-
-    // Check if there are recommendations
-    if (isset($data['results']) && count($data['results']) > 0) {
-        // Extract information from the first recommended item
-        $recommendation = $data['results'][0];
-
-        // Return relevant information
-        return [
-            'title' => $recommendation['title'] ?? $recommendation['name'],
-            'media_type' => $mediaType,
-            'poster_path' => $recommendation['poster_path'] ?? '/default_poster.jpg', // Use a default poster path if not available
-        ];
-    } else {
-        // If no recommendations are found, return a default result
-        return [
-            'title' => 'No Recommendations Found',
-            'media_type' => $mediaType,
-            'poster_path' => '/default_poster.jpg',
-        ];
-    }
-}
-
-// Function to get recent watched recommendations
-function getRecentWatchedRecommendations($username) {
-    global $apiKey; // Use the global API key variable
-    // Get the account ID
-    $accountId = getAccountIDByUsername($username);
-
-    // If account ID is not found or there is an issue, return popular recommendations
-    if (!$accountId) {
-        return getPopularRecommendations();
-    }
-
+function getMoviesByMovieAndGenre($username) {
+    // Global variable for TMDB API key
+    global $apiKey;
     // Connect to the database
     $conn = dbConnect();
-
-    // Select the most recent watched movie
-    $query = "SELECT movie_id, media_type FROM watched_list WHERE account_id = ? ORDER BY timestamp DESC LIMIT 1";
-    $stmt = $conn->prepare($query);
+    // Return an empty array if there is a connection error
+    if ($conn->connect_error) {
+        return [];
+    }
+    // Get the account id with the username
+    $accountId = getAccountIDByUsername($username);
+    // Fetch favorite genre and movie with the account id
+    $sql = "SELECT FavoriteGenres FROM Profiles WHERE AccountID = ?";
+    $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $accountId);
     $stmt->execute();
-    $stmt->bind_result($movieId, $mediaType);
-
-    // Fetch the result
+    $stmt->bind_result($favoriteGenre);
     $stmt->fetch();
-
-    // Close the database connection
     $stmt->close();
-    $conn->close();
-
-    // If there is no recent watched movie, return popular recommendations
-    if (!$movieId) {
-        return getPopularRecommendations();
+    // Return an empty array if the user has no favorite genre
+    if (!$favoriteGenre) {
+        $conn->close();
+        return [];
     }
-
-    // Get recommendation based on the most recently watched movie
-    $recommendation = getRecommendationByMovieId($movieId, $mediaType);
-
-    return $recommendation;
+    // Make API request to TMDB for movies by genre
+    $tmdbApiUrl = 'https://api.themoviedb.org/3/';
+    $tmdbDiscoverEndpoint = 'discover/movie';
+    $apiEndpoint = $tmdbApiUrl . $tmdbDiscoverEndpoint . '?api_key=' . $apiKey . '&with_genres=' . urlencode($favoriteGenre);
+    // Use file_get_contents to make the request
+    $apiResponse = file_get_contents($apiEndpoint);
+    // Return an empty array if the API request has an error
+    if ($apiResponse === false) {
+        $conn->close();
+        return [];
+    }
+    // Decode the JSON response
+    $moviesData = json_decode($apiResponse, true);
+    // Map the results to the desired format
+    $recommendedMovies = array_map(function ($movie) {
+        // Determine media type (movie)
+        $mediaType = 'movie';
+        return [
+            'title' => $movie['title'],
+            'overview' => $movie['overview'],
+            'release_date' => $movie['release_date'],
+            'poster_path' => isset($movie['poster_path']) ? "https://image.tmdb.org/t/p/w500" . $movie['poster_path'] : 'Not available',
+            'MovieID' => $movie['id'],
+            'MediaType' => $mediaType,
+        ];
+    }, $moviesData['results']);
+    // Return the first 10 recommended movies
+    $result = array_slice($recommendedMovies, 0, 10);
+    // Close the database connection
+    $conn->close();
+    return $result;
 }
-
-
+function getAccountIdByUsername($username) {
+    // This will connect to our databse using our credentials. If there is an error, a message is displayed 
+    $conn = dbConnect();
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $username = $conn->real_escape_string($username);
+    // Account ID is associated with the username 
+    $sql = "SELECT AccountID FROM Accounts WHERE Username = '$username'";
+    $result = $conn->query($sql);
+    // If the user name was found it gets the account ID 
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $accountId = $row['AccountID'];
+        $conn->close();
+        return $accountId;
+    // If the username was not found the database connection is closed and returns false
+    } else {
+        $conn->close();
+        return false;
+    }
+}
+function dbConnect() {
+	//attempts connection to the database
+	try{
+	$login = new mysqli("localhost","root","Qg5OKQ","SceneSync");
+	}
+	//if($login -> connect_error){
+	catch(Exception $e){
+	//if error is detected attempts to reconnect
+		//notifying the users of shift
+		echo "Error connecting to primary node: DB Server 1 \n";
+		echo "Reattempting at secondary node: DB Server 2 \n";
+		$login = new mysqli("10.244.1.5","fourth","four4four","SceneSync");
+		//if failure again ends program on cluster failure
+		if ($login -> connect_errno != 0) {
+			echo "Error connecting to secondary node: DB Server 2 \n" . $this -> login -> connect_error . PHP_EOL;
+			echo "DB Cluster Failure: Alert System Administrators \n" . $this -> login -> connect_error . PHP_EOL;
+			exit(1);
+		}
+	}
+	echo "correctly connected to database" . PHP_EOL;
+	return $login;
+}
 $username = 'Billards27';
-$result = getRecentWatchedRecommendations($username);
+$result = $getMoviesByMovieAndGenre($username);
 print_r($result);
-
 
 ?>
